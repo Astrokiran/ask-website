a45c4599bf024205148c14ee030b5df094358f2b
chaithanyaask <chaithanya@pixelforgetechnology>
2025-09-12 07:37:59 +0000

    Hero sections images are added in CMS

--- components/featured-astrologers.tsx
+++ components/featured-astrologers.tsx
@@ -18,7 +18,7 @@ interface Astrologer {
   image: string;
 }
 
-const client = createClient({
+export const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
