0994dffdb6d5ecd084efb20ee867458fd8af5e18
chaithanyaask <chaithanya@pixelforgetechnology>
2025-09-16 11:15:18 +0000

    folder changes for amplify

--- app/amplify.ts
+++
@@ -1,8 +0,0 @@
-// Temporarily commented out Amplify configuration for build
-// import { Amplify } from 'aws-amplify';
-// import { generateClient } from '@aws-amplify/api';
-
-// Placeholder client for build
-const client = null;
-
-export { client };
